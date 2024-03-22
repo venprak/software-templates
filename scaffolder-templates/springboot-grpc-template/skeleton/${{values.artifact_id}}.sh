f9ae892118b4153229909ac2378deaf753fb909e
Venkat <venprak@yahoo.com>
2024-03-22 16:44:53 +0530

    Changes

--- scaffolder-templates/springboot-grpc-template/skeleton/${{values.artifact_id}}.sh
+++ scaffolder-templates/springboot-grpc-template/skeleton/${{values.artifact_id}}.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/bin/java $JVM_DEFAULT_ARGS $JVM_ARGS -jar /usr/share/${{values.artifact_id}}/${{values.artifact_id}}.jar "$@"
+exec java $JVM_DEFAULT_ARGS $JVM_ARGS -jar /usr/share/${{values.artifact_id}}/${{values.artifact_id}}.jar "$@"
